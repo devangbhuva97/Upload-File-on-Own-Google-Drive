0fb9b33ae54824036765aa4e8e1fce627dd45cb1
devangbhuva97 <devangbhuva123@yahoo.com>
2017-06-26 14:26:01 +0530

    initial commit

---
+++ app/Http/Controllers/UploadFileController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Google_Client;
+use Google_Service_Drive;
+use Google_Service_Drive_DriveFile;
+
+
+class UploadFileController extends Controller
+{
+	public function index()
+	{
+		$client = new Google_Client();
+		$client->setAuthConfigFile(__DIR__ . '/client_secret.json');
+		$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback');
+		$client->addScope(Google_Service_Drive::DRIVE);
+		$client->setAccessType('offline');
+		$refreshToken = env('GOOGLE_DRIVE_REFRESH_TOKEN');
+		$client->fetchAccessTokenWithRefreshToken($refreshToken);
+		$client->getAccessToken();
+			
+		$service = new Google_Service_Drive($client);
+		
+		$folderName = "File Upload on Own Google Drive";
+		$files = $service->files->listFiles();
+
+		$found = false;
+
+		foreach ($files as $key=>$value) {
+			if ($files[$key]['name'] == $folderName) {
+				$found = true;
+				$folderId = $files[$key]['id'];
+				break;
+			}
+		}
+		
+		if ($found == false) {
+			$fileMetadata = new Google_Service_Drive_DriveFile(array(
+			'name' => $folderName,
+			'mimeType' => 'application/vnd.google-apps.folder'));
+			$folder = $service->files->create($fileMetadata, array(
+			'fields' => 'id'));
+			$folderId = $folder->id;
+		}
+
+		$fileMetadata = new Google_Service_Drive_DriveFile(array(
+		'name' => 'photo.png',
+		'parents' => array($folderId)));
+
+		$content = file_get_contents(__DIR__ . '/Screenshot (3).png');
+		$file = $service->files->create($fileMetadata, array(
+		  'data' => $content,
+		  'mimeType' => 'image/jpeg',
+		  'uploadType' => 'multipart',
+		  'fields' => 'id'));
+
+		dd($folderId);
+	}
+}
